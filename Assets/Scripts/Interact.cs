6855d226ce3ce335a188c536bbda4efa090b88f1
helmiamsani <48036579+helmiamsani@users.noreply.github.com>
2019-05-09 12:13:35 +1000

    Dialogue Finished

--- Assets/Scripts/Interact.cs
+++ Assets/Scripts/Interact.cs
@@ -1,13 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+
 //this script can be found in the Component section under the option Intro RPG/Player/Interact
 [AddComponentMenu("Intro PRG/RPG/Player/Interact")]
 public class Interact : MonoBehaviour
 {
     #region Variables
     public Image image;
+    public Text[] texts;
+    [Space]
+    public GameObject nextButton, acceptButton, declineButton, finishButton;
+    #endregion
 
+    #region Start
+    private void Start()
+    {
+
+    }
     #endregion
 
     #region Update
@@ -29,6 +39,9 @@ public class Interact : MonoBehaviour
                 //and that hits info is tagged NPC
                 if (hitInfo.collider.CompareTag("NPC"))
                 {
+                    nextButton.SetActive(true);
+                    texts[0].enabled = true;
+                    image.enabled = true;
                     Movement.canMove = false;
                     //Debug that we hit a NPC
                     Debug.Log("You are talking to NPC");
@@ -44,6 +57,45 @@ public class Interact : MonoBehaviour
                 #endregion
             }
         }
+    }
+    #endregion
+
+    #region Button Manager
+    public void NextButton()
+    {
+        texts[0].enabled = false;
+        texts[1].enabled = true;
+        nextButton.SetActive(false);
+        acceptButton.SetActive(true);
+        declineButton.SetActive(true);
+    }
+
+    public void AcceptButton()
+    {
+        texts[1].enabled = false;
+        texts[2].enabled = true;
+        acceptButton.SetActive(false);
+        declineButton.SetActive(false);
+        finishButton.SetActive(true);
+    }
+
+    public void DeclineButton()
+    {
+        texts[1].enabled = false;
+        texts[3].enabled = true;
+        acceptButton.SetActive(false);
+        declineButton.SetActive(false);
+        finishButton.SetActive(true);
+    }
+
+    public void FinishButton()
+    {
+        texts[1].enabled = false;
+        texts[2].enabled = false;
+        texts[3].enabled = false;
+        finishButton.SetActive(false);
+        image.enabled = false;
+        Movement.canMove = true;
     }
     #endregion
 }
